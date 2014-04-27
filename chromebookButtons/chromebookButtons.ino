8730b2f1828708815ae3d86b0676f0e8201a245a
David Hay <misterhay@gmail.com>
2014-04-27 00:07:47 -0600

    corrected some typos, added email and password strings

--- chromebookButtons/chromebookButtons.ino
+++ chromebookButtons/chromebookButtons.ino
@@ -1,27 +1,26 @@
 // send a email and password when button is pressed
-var email = "example@example.com";
-var password = "thisisaweakpassword";
-var enrolButton = 2;
-var loginButton = 3;
-var enrolLED = 4;
-var loginLED = 5;
+String email = "example@example.com";
+String password = "thisisaweakpassword";
+const int enrolButton = 2;
+const int loginButton = 3;
+const int enrolLED = 11;
+const int loginLED = 13;
 
 void setup() {
   pinMode(enrolButton, INPUT_PULLUP); // consider debouncing these
   pinMode(loginButton, INPUT_PULLUP);
   pinMode(enrolLED, OUTPUT);
   pinMode(loginLED, OUTPUT);
-  // do I need to add a delay here while the computer sets up?
   digitalWrite(enrolLED, HIGH);
-  digitalWrite(loginLED, HIGH)
+  digitalWrite(loginLED, HIGH);
 }
 
 void loop() {
-  if(digitalRead(enrolButton)==LOW_){
-    i = enrol();
+  if(digitalRead(enrolButton)==LOW){
+    enrol();
   }
-  if(digitalRead(loginButton)==LOW_){
-    i = login();
+  if(digitalRead(loginButton)==LOW){
+    login();
   }
 }
 
@@ -30,6 +29,10 @@ void enrol() {
   // then ctrl-alt-e to enrol
   // then enter twice (with delays) to accept the agreements
   Keyboard.begin();
+  //Keyboard.print(KEY_ENTER);
+  //Keyboard.print(KEY_DOWN);
+  //Keyboard.print(KEY_TAB);
+  //delay(1000);
   Keyboard.press(KEY_LEFT_CTRL);
   Keyboard.press(KEY_LEFT_ALT);
   Keyboard.press('e');
