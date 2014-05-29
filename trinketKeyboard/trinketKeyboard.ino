5c6bd22a2cc0359a9959f6f0e839caeefe4931d5
David Hay <misterhay@gmail.com>
2014-05-29 15:30:43 -0600

    turned on the LED when the keyboard is outputting

--- trinketKeyboard/trinketKeyboard.ino
+++ trinketKeyboard/trinketKeyboard.ino
@@ -7,16 +7,19 @@ int blackButton = 2;
 void setup() {
   pinMode(redButton, INPUT_PULLUP); // button pins as inputs with internal pullup resistors
   pinMode(blackButton, INPUT_PULLUP);
+  pinMode(LED, OUTPUT);
   TrinketKeyboard.begin(); // start USB keyboard
 }
 
 void redButtonFunction() {
+  digitalWrite(LED, HIGH);
   TrinketKeyboard.pressKey(KEYCODE_MOD_LEFT_SHIFT, KEYCODE_A);
   TrinketKeyboard.pressKey(0, 0);
   delay(100);
 }
 
 void blackButtonFunction() {
+  digitalWrite(LED, HIGH);
   TrinketKeyboard.print("Hello World!");
 }
 
@@ -25,4 +28,5 @@ void loop() {
   delay(5); // to help debounce the inputs
   if (digitalRead(redButton) == LOW) {redButtonFunction();} // if pressed call the firstButton function
   if (digitalRead(blackButton) == LOW) {blackButtonFunction();}
+  digitalWrite(LED, LOW);
 }
