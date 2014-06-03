10394c153dbf4bc418fcc27304f26a40fd868f1f
David Hay <misterhay@gmail.com>
2014-06-03 15:36:45 -0600

    changed the variable and function naming

--- chromebookButtons/chromebookButtons.ino
+++ chromebookButtons/chromebookButtons.ino
@@ -1,32 +1,25 @@
-// send a email and password when button is pressed
-
-String email = "example@example.com";
-String password = "thisisaweakpassword@";
-const int enrolButton = 2;
-const int loginButton = 3;
-const int enrolLED = 11;
-const int loginLED = 13;
+const int redButton = 0;
+const int blackButton = 1;
+const int LED = 11;
 
 #include <Bounce.h>
-Bounce button1 = Bounce(enrolButton, 10); // 10 ms debouce
-Bounce button2 = Bounce(loginButton, 10);
+Bounce redButtonDebounce = Bounce(redButton, 10); // 10 ms debouce
+Bounce blackButtonDebounce = Bounce(blackButton, 10);
 void setup() {
-  pinMode(enrolButton, INPUT_PULLUP);
-  pinMode(loginButton, INPUT_PULLUP);
-  pinMode(enrolLED, OUTPUT);
-  pinMode(loginLED, OUTPUT);
-  digitalWrite(enrolLED, HIGH);
-  digitalWrite(loginLED, HIGH);
+  pinMode(redButton, INPUT_PULLUP);
+  pinMode(blackButton, INPUT_PULLUP);
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
-  button1.update();
-  button2.update();
-  if(button1.fallingEdge()) {enrol();}
-  if(button2.fallingEdge()) {login();}
+  digitalWrite(LED, HIGH);  
+  redButtonDebounce.update();
+  blackButtonDebounce.update();
+  if(redButtonDebounce.fallingEdge()) {redButtonFunction();}
+  if(blackButtonDebounce.fallingEdge()) {blackButtonFunction();}
 }
 
-void enrol() {
+void redButtonFunction() {
   // key sequence to select the keyboard type, press enter,
   // then ctrl-alt-e to enrol
   // then enter twice (with delays) to accept the agreements
@@ -35,38 +28,38 @@ void enrol() {
   //Keyboard.print(KEY_DOWN);
   //Keyboard.print(KEY_TAB);
   //delay(1000);
-  Keyboard.press(KEY_LEFT_CTRL);
+  /*Keyboard.press(KEY_LEFT_CTRL);
   Keyboard.press(KEY_LEFT_ALT);
   Keyboard.press('e');
   delay(50);
   Keyboard.releaseAll();
-  delay(3000);
-  Keyboard.print(email);
+  delay(3000);//*/
+  Keyboard.print("this is the red button");
   Keyboard.press(KEY_TAB); //tab to get to the password field
   delay(50);
   Keyboard.releaseAll();
-  Keyboard.print(password);
+  Keyboard.print("this is also a test");
   Keyboard.press(KEY_ENTER);
   delay(50);
   Keyboard.releaseAll();
   
   Keyboard.end();
-  digitalWrite(enrolLED, LOW);
+  digitalWrite(LED, LOW);
   delay(1000);
 }
 
-void login() {
+void blackButtonFunction() {
   Keyboard.begin();
-  Keyboard.print(email);
+  Keyboard.print("I'm testing this black button");
   Keyboard.press(KEY_TAB); //tab to get to the password field
   delay(50);
   Keyboard.releaseAll();
-  Keyboard.print(password);
+  Keyboard.print("this is also a test");
   Keyboard.press(KEY_ENTER);
   delay(50);
   Keyboard.releaseAll();
   Keyboard.end();
-  digitalWrite(loginLED, LOW);
+  digitalWrite(LED, LOW);
   delay(1000);
   Keyboard.press(KEY_TAB);
   delay(50);
@@ -87,5 +80,4 @@ void login() {
   Keyboard.press(KEY_SPACE);
   delay(50);
   Keyboard.releaseAll();
-
 }
